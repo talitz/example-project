c66aea5084a42bb07d4dc7fe058fa02c5fdcd8aa
Haakon Magne Holmen <haakonholmen@hotmail.com>
2019-09-01 17:43:51 +0200

    Added functionality and docstring

--- example/example_code.py
+++ example/example_code.py
@@ -1,2 +1,25 @@
-def function():
-    print('This is a practice project!')
+from random import randint
+
+
+def random_number_generator(start, stop):
+    """Returns a random number between start and stop.
+
+    Parameters
+    ----------
+    start : int
+        Lowest possible number.
+    stop : int
+        Highest possible number.
+
+    Returns
+    -------
+    int
+        A random integer between start and stop.
+
+    Examples
+    --------
+    >>> random_number_generator(0, 10)
+    5  #random
+
+    """
+    return randint(start, stop)
